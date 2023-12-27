fe2e7483cbcca90d19077e9126502036f4227d3d
Manikandan-Mannai <iamanikandan.mannai@gmail.com>
2023-12-27 16:38:41 +0530

    animation added

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,39 +1,57 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import Dove from '../assets/dove.png';
 
-const StyledAboutUsSection = styled.section`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 50px;
+const fadeInLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
 `;
 
-const Left = styled.div`
-  flex: 1;
-  text-align: center;
+const fadeInRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
 `;
 
-const Right = styled.div`
-  flex: 1;
-  padding-left: 50px;
+const fadeInImage = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
 `;
 
 const AboutUs = () => {
     return (
         <StyledAboutUsSection>
             <Left>
-                <h1>hello</h1>
+                <img src={Dove} alt="Dove" />
             </Left>
             <Right>
                 <h1>About Us</h1>
                 <p>
-                    Discover the essence of elevated living through K&L Life Spaces, an integral subsidiary within the esteemed
-                    Nakshatra Builders and HavinHomes Realty family. As specialists in property development and construction, we
-                    redefine spaces into captivating narratives of modern luxury. Our unwavering dedication to craftsmanship and
-                    innovation drives us to curate living experiences that transcend the ordinary. With a solid foundation of
-                    expertise from our parent companies, we turn blueprints into realities, and aspirations into addresses that
-                    resonate with sophistication. Join us in creating a future where each structure we build is infused with
-                    passion, and every space becomes a testament to our commitment to crafting exceptional lifestyles.
+                    Discover the essence of elevated living through <span>K&L Life Spaces</span>, an integral subsidiary within the
+                    esteemed <span>Nakshatra Builders</span> and <span>HavinHomes Realty family</span>. As specialists in property
+                    development and construction, we redefine spaces into captivating narratives of modern luxury. Our unwavering
+                    dedication to craftsmanship and innovation drives us to curate living experiences that transcend the ordinary.
+                    With a solid foundation of expertise from our parent companies, we turn blueprints into realities, and
+                    aspirations into addresses that resonate with sophistication. Join us in creating a future where each structure
+                    we build is infused with passion, and every space becomes a testament to our commitment to crafting exceptional
+                    lifestyles.
                 </p>
             </Right>
         </StyledAboutUsSection>
@@ -41,3 +59,52 @@ const AboutUs = () => {
 };
 
 export default AboutUs;
+
+const StyledAboutUsSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    text-align: center;
+  }
+`;
+
+const Left = styled.div`
+  flex: 1;
+
+  img {
+    width: 100%;
+    animation: ${fadeInImage} 1s ease-in-out;
+  }
+
+  @media only screen and (max-width: 768px) {
+    margin-bottom: 10px;
+
+    img {
+      width: 100%;
+    }
+  }
+`;
+
+const Right = styled.div`
+  flex: 1;
+  animation: ${fadeInRight} 1s ease-in-out;
+
+  h1 {
+    font-size: 36px;
+    margin-bottom: 10px;
+  }
+
+  span {
+    color: #7065f0;
+    font-weight: bold;
+  }
+
+  @media only screen and (max-width: 768px) {
+    font-size: 12px;
+    animation: ${fadeInLeft} 1s ease-in-out;
+  }
+`;
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import BackgroundVideo from '../assets/video 1.mp4';
 
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
 
 const LandingPage = () => {
   return (
     <>
       <Hero>
-        <Video src={BackgroundVideo} autoPlay loop muted playsInline></Video>
+        <VideoContainer>
+          <Video src={BackgroundVideo} autoPlay loop muted playsInline></Video>
+          <Overlay />
+        </VideoContainer>
         <Content>
-          <h1>The best place to find</h1>
+          <h1 style={{ color: '#fff' }}>The best place to find</h1>
           <h1 style={{ color: '#7065F0' }}>Your dream house</h1>
         </Content>
       </Hero>
@@ -27,6 +38,12 @@ const Hero = styled.section`
   margin: auto;
 `;
 
+const VideoContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
 const Video = styled.video`
   object-fit: cover;
   width: 100%;
@@ -39,6 +56,18 @@ const Video = styled.video`
   border-bottom-right-radius: 16px;
 `;
 
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.7); /* Adjust the alpha value for darkness */
+  z-index: 1;
+    border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 16px;
+`;
+
 const Content = styled.div`
   position: absolute;
   top: 50%;
@@ -47,4 +76,15 @@ const Content = styled.div`
   text-align: center;
   color: var(--secondary-color);
   z-index: 2;
+  animation: ${fadeIn} 1.5s ease-in-out;
+
+  h1 {
+    font-size: 66px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    h1 {
+      font-size: 26px;
+    }
+  }
 `;
