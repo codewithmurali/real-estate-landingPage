9e6a55f7ee63aefb7dd4305af1693c6fbef24434
codewithmurali <muralikumar.admire@gmail.com>
2023-12-29 09:47:31 +0530

    After some corrections in the streetview

--- src/components/StreetView.jsx
+++ src/components/StreetView.jsx
@@ -1,72 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import Slider from 'react-slick';
-import { streetViewData } from '../data';
-import styled from 'styled-components';
+import React, { useState, useEffect } from "react";
+import Slider from "react-slick";
+import { streetViewData } from "../data";
+import styled from "styled-components";
 
 // Import left and right arrow icons
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const StreetView = () => {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        setData(streetViewData);
-    }, []);
+  useEffect(() => {
+    setData(streetViewData);
+  }, []);
 
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        centerMode: true,
-        centerPadding: '20px',
-        focusOnSelect: true,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: false,
-                },
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    initialSlide: 2,
-                },
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                },
-            },
-        ],
-    };
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    centerMode: true,
+    centerPadding: "20px",
+    focusOnSelect: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
-    return (
-        <Container id="streetview">
-            <h1>STREET VIEW</h1>
-            <Slider {...settings}>
-                {data.map((item, index) => (
-                    <Image key={index}>
-                        <img src={item.url} alt={item.name} />
-                        <h3>{item.name}</h3>
-                    </Image>
-                ))}
-            </Slider>
-        </Container>
-    );
+  return (
+    <Container id="streetview">
+      <h1>STREET VIEW</h1>
+      <Slider {...settings}>
+        {data.map((item, index) => (
+          <Image key={index}>
+            <img src={`${item.url}`} alt={item.name} />
+            <h3>{item.name}</h3>
+          </Image>
+        ))}
+      </Slider>
+    </Container>
+  );
 };
 
 const ArrowIcon = styled.div`
@@ -79,33 +79,32 @@ const ArrowIcon = styled.div`
 `;
 
 const NextArrow = ({ onClick }) => (
-    <ArrowIcon style={{ right: 0 }} onClick={onClick}>
-        <IoIosArrowForward />
-    </ArrowIcon>
+  <ArrowIcon style={{ right: 0 }} onClick={onClick}>
+    <IoIosArrowForward />
+  </ArrowIcon>
 );
 
 const PrevArrow = ({ onClick }) => (
-    <ArrowIcon style={{ left: 0 }} onClick={onClick}>
-        <IoIosArrowBack />
-    </ArrowIcon>
+  <ArrowIcon style={{ left: 0 }} onClick={onClick}>
+    <IoIosArrowBack />
+  </ArrowIcon>
 );
-
 
 export default StreetView;
 
 const Container = styled.div`
-padding: 20px 100px;
-.slick-track {
+  padding: 20px 100px;
+  .slick-track {
     display: flex;
-}
-h1{
+  }
+  h1 {
     text-align: right;
-}
-@media only screen and (max-width: 768px) {
-      padding: 50px ;
-      h1{
-        text-align: center;
-      }
+  }
+  @media only screen and (max-width: 768px) {
+    padding: 50px;
+    h1 {
+      text-align: center;
+    }
   }
 `;
 
@@ -123,5 +122,3 @@ const Image = styled.div`
     border-radius: 16px;
   }
 `;
-
-
